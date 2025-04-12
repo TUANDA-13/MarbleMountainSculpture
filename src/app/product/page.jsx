cc72c261d81e45a5cf72a22ee26ce3d3cddeeec3
Tuan Duong <tuan.duong@madison-technology.com>
2025-04-12 19:56:40 +0700

    fix susspense when use useUsearchParams

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -1,20 +1,23 @@
 import { ProductHeader } from '@/components/product/ProductHeader';
 import { ProductList } from '@/components/product/ProductList';
 import { ProductSidebar } from '@/components/product/ProductSidebar';
+import { Suspense } from 'react';
 
 const ProductPage = () => {
   return (
-    <div className="container">
-      <ProductHeader />
-      <div className="flex gap-6 my-10">
-        <div className="w-[25%] shrink-0 pr-6 border-r">
-          <ProductSidebar />
-        </div>
-        <div>
-          <ProductList />
+    <Suspense fallback={<div></div>}>
+      <div className="container">
+        <ProductHeader />
+        <div className="flex gap-6 my-10">
+          <div className="w-[25%] shrink-0 pr-6 border-r">
+            <ProductSidebar />
+          </div>
+          <div>
+            <ProductList />
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
