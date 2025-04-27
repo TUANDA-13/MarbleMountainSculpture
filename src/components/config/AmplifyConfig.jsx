9ff89b8a614cb06f7bd9b6fdc82d24cfd52ec350
Pyke Duong <pyke.duong@avepoint.com>
2025-04-27 12:11:55 +0700

    fix

--- src/components/config/AmplifyConfig.jsx
+++ src/components/config/AmplifyConfig.jsx
@@ -1,8 +1,6 @@
 import { Amplify } from 'aws-amplify';
 import outputs from '../../../amplify_outputs.json';
 
-const amplifyConfig = parseAmplifyConfig(outputs);
-
 Amplify.configure(outputs, { ssr: true });
 
 export default function AmplifyConfig() {
